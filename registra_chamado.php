479917b85283376f48ccfc2ad616e47f7f98b2e5
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-10-22 20:59:57 -0300

    Implementado um script para criar e gravar as asberturas de chamado em formato txt

---
+++ registra_chamado.php
@@ -0,0 +1,22 @@
+<?php 
+echo '<pre>';
+    print_r($_POST);
+echo '</pre>';
+
+// trabalhando na montagem do texto
+$titulo = str_replace('#','-',$_POST['titulo']);
+$categoria = str_replace('#','-',$_POST['categoria']);
+$descricao = str_replace('#','-',$_POST['descricao']);
+
+$texto = $titulo .' # '. $categoria .' # '. $descricao. PHP_EOL;
+
+// abrindo o arquivo txt
+$arquivo = fopen('arquivo.txt','a');
+// escrevendo o tetxo
+fwrite($arquivo, $texto);
+// fechando o arquivo
+fclose($arquivo);
+header('Location: abrir_chamado.php');
+
+
+?>
