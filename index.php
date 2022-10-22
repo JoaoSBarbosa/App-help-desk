2da178d5e45cb192e16ade034c8e07fae9e59eef
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-10-22 20:20:39 -0300

    Incluido a função para encerrar sessão (logoff) com session_destroy();

--- index.php
+++ index.php
@@ -18,9 +18,12 @@
 
 <body>
 
-<?php 
- include_once "menu.php";
-?>
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      App Help Desk
+    </a>
+  </nav>
 
   <div class="container">
     <div class="row">
--- menu.php
+++ menu.php
@@ -3,4 +3,9 @@
       <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="logoff.php">SAIR</a>
+    </li>
+    </ul>
   </nav>
