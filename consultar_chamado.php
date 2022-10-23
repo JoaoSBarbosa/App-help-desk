adf69d34c0fd6010434f53a5a0837f607d4386bd
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-10-22 21:37:21 -0300

    Implementado o script para consultar os chamados abertos

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,11 +1,22 @@
+<?php require_once("validador_acesso.php"); ?>
 <?php
-require_once("validador_acesso.php");
+// Array armazenando os chamados
+$chamados = array();
+$arquivo = fopen('arquivo.txt', 'r');
+// percorrer o arquivo.txt enquanto tiver registros
+while (!feof($arquivo)) {
+  $registros = fgets($arquivo);
+  $chamados[] = $registros;
+}
+// fechar o arquivo
+fclose($arquivo);
 ?>
 
 <html>
 
 <head>
   <meta charset="utf-8" />
+
   <title>App Help Desk</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -20,40 +31,37 @@ require_once("validador_acesso.php");
 </head>
 
 <body>
-
-  <?php
-  include_once "menu.php";
+  <?php 
+    include_once "menu.php"; 
   ?>
 
   <div class="container">
     <div class="row">
-
       <div class="card-consultar-chamado">
         <div class="card">
           <div class="card-header">
             Consulta de chamado
           </div>
-
           <div class="card-body">
 
-            <div class="card mb-3 bg-light">
-              <div class="card-body">
-                <h5 class="card-title">Título do chamado...</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                <p class="card-text">Descrição do chamado...</p>
-
+            <?php
+            foreach ($chamados as $chamado) { ?>
+              <?php
+              $chamado_dados = explode('#', $chamado);
+              if (count($chamado_dados) < 3) {
+                continue;
+              }
+              ?>
+              <div class="card mb-3 bg-light">
+                <div class="card-body">
+                  <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                  <p class="card-text"><?= $chamado_dados[2] ?></p>
+                </div>
               </div>
-            </div>
-
-            <div class="card mb-3 bg-light">
-              <div class="card-body">
-                <h5 class="card-title">Título do chamado...</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                <p class="card-text">Descrição do chamado...</p>
-
-              </div>
-            </div>
-
+            <?php
+            }
+            ?>
             <div class="row mt-5">
               <div class="col-6">
                 <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
