5cd4f63f5ca98e5e9433c242acfc7e6fdb3c55f6
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-10-22 22:09:41 -0300

    Definido um script para controle de acesso aos chamados

--- valida_login.php
+++ valida_login.php
@@ -6,28 +6,33 @@ session_start();
 
 // variavel que verifica se a autentificação foi realizada
 $usuario_autentificado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
+$perfis = array(1 => 'administrativo', 2 => 'usuario');
 
 $usuarios_app = array(
-    array(
-        'email' => 'adm@teste.com.br',
-        'senha' => 123456
-    ),
-    array(
-        'email' => 'user@teste.com.br',
-        'senha' => 'abcd'
-    ),
+    array('id' => 1,'email' => 'adm@teste.com.br','senha' => 1234, 'perfil_id' => 1),
+    array('id' => 2,'email' => 'user@teste.com.br','senha' => 1234, 'perfil_id' => 1),
+    array('id' => 3,'email' => 'jose@teste.com.br','senha' => 1234, 'perfil_id' => 2),
+    array('id' => 4,'email' => 'maria@teste.com.br','senha' => 1234, 'perfil_id' => 2)
 );
 
 foreach($usuarios_app as $user){
 
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autentificado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
+
     }
 }
 // função de enviar verificar autenticação e retornar o status
 if($usuario_autentificado){
     echo 'Usuario autentificado';
     $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['id'] = $usuario_id;
+    $_SESSION['perfil_id'] = $usuario_perfil_id;
+
     header('Location: home.php');
 }else {
     header('Location:index.php?login=erro');
